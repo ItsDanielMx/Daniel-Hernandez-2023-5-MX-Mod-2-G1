67c67bd8426f61fe50c88d4a1399c93c17fbb8ae
Daniel Hernandez <danielhernandez09012002@gmail.com>
2023-06-16 19:08:33 -0600

    Tarea 1

--- game/components/game.py
+++ game/components/game.py
@@ -1,7 +1,6 @@
 import pygame
-
 from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
-
+from game.components.spaceship import Spaceship
 
 class Game:
     def __init__(self):
@@ -11,12 +10,12 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
-        self.game_speed = 10 # el numero de pixeles que el "objeto / imagen" se mueve en patalla
+        self.game_speed = 10
         self.x_pos_bg = 0
         self.y_pos_bg = 0
+        self.spaceship = Spaceship()
 
     def run(self):
-        # Game loop: events - update - draw
         self.playing = True
         while self.playing:
             self.handle_events()
@@ -28,25 +27,34 @@ class Game:
         pygame.quit()
 
     def handle_events(self):
-        # pygame.event.get() es un iterable (lista)
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: #el QUIT event es el click en el icono que cierra ventana
+            if event.type == pygame.QUIT: 
                 self.playing = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.spaceship.move_left()
+                elif event.key == pygame.K_RIGHT:
+                    self.spaceship.move_right()
 
     def update(self):
-        pass # pass equivale a hacer nada 
+        events = pygame.key.get_pressed() 
+        self.spaceship.update(events)
 
     def draw(self):
-        self.clock.tick(FPS) # configuro cuantos frames per second voy a dibujar
-        self.screen.fill((255, 255, 255)) # lleno el screen de color BLANCO???? 255, 255, 255 es el codigo RGB
+        self.clock.tick(FPS) 
+        self.screen.fill((255, 255, 255)) 
+        
         self.draw_background()
-        pygame.display.update() # esto hace que el dibujo se actualice en el display de pygame
-        pygame.display.flip()  # hace el cambio
+
+        self.spaceship.draw(self.screen)
+
+        pygame.display.update() 
+        pygame.display.flip()  
 
     def draw_background(self):
         image = pygame.transform.scale(BG, (SCREEN_WIDTH, SCREEN_HEIGHT))
-        image_height = image.get_height()# alto de la imagen
-        self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg)) # blit "dibuja"
+        image_height = image.get_height()
+        self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg)) 
         self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg - image_height))
         if self.y_pos_bg >= SCREEN_HEIGHT:
             self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg - image_height))
